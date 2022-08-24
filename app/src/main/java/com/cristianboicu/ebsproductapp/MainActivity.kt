7712e099f42c6344f40d21b74f7f7cf9e6fa596d
Cristian <67103037+cristianboicu@users.noreply.github.com>
2022-08-24 20:03:38 +0300

    Sets up toolbar with navigation graph

--- app/src/main/java/com/cristianboicu/ebsproductapp/MainActivity.kt
+++ app/src/main/java/com/cristianboicu/ebsproductapp/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.cristianboicu.ebsproductapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +15,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration =
+            AppBarConfiguration(topLevelDestinationIds = setOf())
+
+        setUpToolBar(navController, appBarConfiguration, toolbar)
+
+    }
+
+    private fun setUpToolBar(
+        navController: NavController,
+        appBarConfiguration: AppBarConfiguration,
+        toolbar: Toolbar,
+    ) {
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.productsFragment) {
+                toolbar.setNavigationIcon(R.drawable.ic_user)
+            }
+        }
     }
 }
