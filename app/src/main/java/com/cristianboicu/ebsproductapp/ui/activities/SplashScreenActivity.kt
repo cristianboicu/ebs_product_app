244bb215108226a2adced1501b232f4d30ee31c4
Cristian <67103037+cristianboicu@users.noreply.github.com>
2022-08-29 12:53:13 +0300

    code refactoring

---
+++ app/src/main/java/com/cristianboicu/ebsproductapp/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.cristianboicu.ebsproductapp.ui.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+
+}
